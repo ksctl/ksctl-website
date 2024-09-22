2e8ce8b71c876b265d929a24dbb43507421af9bb
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-09-22 19:36:02 +0530

    feat: added ci for the prod deploy
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ksctl.com/',
+  site: 'https://ksctl.com',
   integrations: [tailwind()]
 });
