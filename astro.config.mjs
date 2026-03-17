63acde0110d360cf91f3222bf4674f417880822c
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2026-03-17 22:22:30 +0530

    removal of react as deps and motion, shadcn
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ksctl.com',
-  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
