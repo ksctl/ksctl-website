99f60d6d0d4e2e4d35a28cab45d3e6072a461f90
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2026-04-28 22:54:05 +0530

    updates for seo
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ksctl.com',
-  integrations: [alpinejs(), icon()],
+  trailingSlash: 'ignore',
+  integrations: [
+    alpinejs(),
+    icon(),
+    sitemap({
+      lastmod: new Date(),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
