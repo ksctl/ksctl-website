057ebd566f6a61a513afe1e86d139242010f27ae
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2026-07-03 00:23:04 +0530

    updates to the dead code css
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,10 @@
 
 export default new Map([
-["src/content/blog/kli-v23-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkli-v23-release.mdx&astroContentModuleFlag=true")],
-["src/content/blog/kli-v221-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkli-v221-release.mdx&astroContentModuleFlag=true")],
-["src/content/blog/kli-v2-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkli-v2-release.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/k8s-cluster-autoscaling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fk8s-cluster-autoscaling.mdx&astroContentModuleFlag=true")],
+["src/content/blog/kli-v2-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkli-v2-release.mdx&astroContentModuleFlag=true")],
+["src/content/blog/kli-v221-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkli-v221-release.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/kli-v24-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkli-v24-release.mdx&astroContentModuleFlag=true")],
+["src/content/blog/kli-v23-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkli-v23-release.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/kli-v25-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkli-v25-release.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/kli-v26-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkli-v26-release.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/kli-v27-release.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkli-v27-release.mdx&astroContentModuleFlag=true")],
